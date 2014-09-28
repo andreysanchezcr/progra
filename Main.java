f2592a2d4dd673384efd236c5c3de08ad34480e2
Nenis <sear12@hotmail.es>
2014-09-27 18:50:43 -0600

    Archivo que inicializa la ventana principal y todos sus componentes

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+package tp2;
+
+/**
+ *
+ * @author Familia Alpizar R
+ */
+public class TP2 {
+
+    public static void main(String[] args) {
+        new VentanaPrincipal().setVisible(true);
+    }
+    
+}
